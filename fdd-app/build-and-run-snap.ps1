cf93ca0a52efeb9ae18693b6b7a01558bec86688
MichaelSimons <msimons@microsoft.com>
2019-09-12 14:23:17 -0500

    wip

--- fdd-app/build-and-run-snap.ps1
+++ fdd-app/build-and-run-snap.ps1
@@ -1,6 +1,5 @@
 & dotnet publish -c release -o ./out
 & snapcraft
 
-# --devmode is not necessary
-& sudo snap install --dangerous --devmode fdd-app_0.1_amd64.snap
+& sudo snap install --dangerous fdd-app_0.1_amd64.snap
 & fdd-app
